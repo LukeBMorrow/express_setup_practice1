7b1173c7694a9a1236d0bd7c91a2b62fdbff89c9
LukeBMorrow <lukebmorrow@gmail.com>
2019-07-23 18:34:14 -0500

    attached bootstrap and added a grid system to the default route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router({mergeParams: true});
 
 router.get("/", function(req, res){
-    res.send("landing page here");
+    res.render("index.ejs");
 });
 
 
